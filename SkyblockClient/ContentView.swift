0307258699e441f0c9047546f9b6edba28d98b22
stef-the <75397677+stef-the@users.noreply.github.com>
2021-02-22 09:38:30 -0800

    Initial Commit

---
+++ SkyblockClient/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SkyblockClient
+//
+//  Created by Stefan Leung on 2/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
